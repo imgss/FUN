6fb6fcdf131ebf3baf47689e6a38f4a92e1ec18a
imgss <838632526@qq.com>
2017-05-16 22:47:32 +0800

    调整结构

--- src/components/Posts.vue
+++ src/components/Posts.vue
@@ -4,8 +4,9 @@
       <main class="mdl-layout__content">
           <div class='demo-blog__posts mdl-grid'>
               <div class="mdl-card mdl-cell mdl-cell--12-col" v-for='card in cards'>
-                  <div class="mdl-card__title mdl-card__media mdl-color-text--grey-50">{{card.title}}</div>
+                  <div class="mdl-card__title title mdl-card__media mdl-color-text--grey-50">{{card.title}}</div>
                   <div class="mdl-card__supporting-text meta mdl-color-text--grey-600">{{card.text}}</div>
+                  <div class="mdl-card__supporting-text meta mdl-color-text--grey-600">{{card.postDate}}</div>
               </div>
           </div>
     </main>
@@ -19,13 +20,16 @@ export default {
     return {
       cards: [{
         title: '文章一',
-        text: 'xxxxxxxxxxxxxxxxxxxx33333333333333333333333333333333333333333333333333333333'
+        text: '天边夕阳再次迎上我的脸庞',
+        postDate: '昨天'
       }, {
         title: '文章二',
-        text: 'xxxxxxxxxxxxxxxxxxxx222222222222222222222222222222'
+        text: '再次印着我那不安的新',
+        postDate: '今天'
       }, {
         title: '文章三',
-        text: 'xxxxxxxxxxxxxxxxxxxx'
+        text: '这是什么地方依然是如此的苍凉',
+        postDate: '明天'
       }]
     }
   }
@@ -34,6 +38,10 @@ export default {
 </script>
 
 <style scoped lang='stylus'>
+.mdl-layout__container
+  background: #6734ba
+.title
+  font-size: 34px
 .posts
   display: flex
   justify-content: center
