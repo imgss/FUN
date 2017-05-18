7aab129d0d366aca098ea3dacec3b0b10e61a900
imgss <838632526@qq.com>
2017-05-18 23:39:51 +0800

    简单过度效果

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <template>
   <div id="app">
-    <router-view></router-view>
+    <transition name="xxx">
+      <router-view></router-view>
+    </transition>
   </div>
 </template>
 
@@ -18,5 +20,15 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+}
+.xxx-enter-active {
+  transition: all .3s ease;
+}
+.xxx-leave-active {
+  transition: all .8s cubic-bezier(1.0, 0.5, 0.8, 1.0);
+}
+.xxx-fade-enter,.xxx-leave-active {
+  transform: translateX(10px);
+  opacity: 0;
 }
 </style>
