1346cc9255226209a32f031868b938eba52acbdb
imgss <838632526@qq.com>
2017-05-18 23:52:04 +0800

    初步请求数据ok

--- src/components/article.vue
+++ src/components/article.vue
@@ -80,6 +80,45 @@
 
 
 <script>
+import axios from 'axios'
+export default {
+  data () {
+    return {
+      loading: false,
+      post: null,
+      error: null
+    }
+  },
+  created () {
+    // 组件创建完后获取数据，
+    // 此时 data 已经被 observed 了
+    this.fetchData()
+  },
+  watch: {
+    // 如果路由有变化，会再次执行该方法
+    '$route': 'fetchData'
+  },
+  methods: {
+    fetchData () {
+      this.error = this.post = null
+      this.loading = true
+      // replace getPost with your data fetching util / API wrapper
+      getPost(this.$route.params.id, (err, post) => {
+        this.loading = false
+        if (err) {
+          this.error = err.toString()
+        } else {
+          this.post = post
+        }
+      })
+      function getPost () {
+        axios.get('https://raw.githubusercontent.com/imgss/mdblog/master/1.md').then(function (response) {
+          console.log(response)
+        })
+      }
+    }
+  }
+}
 
 </script>
 
